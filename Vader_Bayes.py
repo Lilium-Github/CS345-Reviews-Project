4285a04294cc7e9d9a21866a94cb2dbf1025633f
Lily <66401081+Lilium-Github@users.noreply.github.com>
2026-04-23 22:00:56 -0600

    Update Vader_Bayes.py
    make VaderBayes its own class

--- Vader_Bayes.py
+++ Vader_Bayes.py
@@ -7,17 +7,47 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import FunctionTransformer
-from sklearn.naive_bayes import MultinomialNB
 from sklearn.feature_extraction.text import CountVectorizer
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from sklearn.preprocessing import MinMaxScaler
 import nltk
 # nltk.download('vader_lexicon')
 
+class VaderBayes:
+    def __init__(self):  
+        # step 6: set everything up for the pipeline
+        text_transformer = CountVectorizer(stop_words='english')
+
+        vader_pipeline = Pipeline([
+            ('reshape', FunctionTransformer(lambda x: x.values.reshape(-1, 1), validate=False)),     # bayes doesn't like negatives, fixing that here
+            ('scale', MinMaxScaler())
+        ])
+
+        preprocessor = ColumnTransformer(
+            transformers=[
+                ('text', text_transformer, 'text'),
+                ('vader', vader_pipeline, 'vader_score')
+            ]
+        )
+
+        # step 7: create the naive bayes model
+        self.model = Pipeline([
+            ('features', preprocessor),
+            ('classifier', MultinomialNB())
+        ])
+
+    def fit(self, X, y):
+        self.model.fit(X,y)
+    
+    def predict(self, X):
+        return self.model.predict(X)
+
+# _______________________________________________________________________________________
+
 # step 1: extract data from the json file
 text_arr = []
 rating_arr = []
-counter = 5000
+counter = 1000
 
 file = "Software.jsonl"
 with open(file, 'r') as fp:
@@ -45,32 +75,12 @@ df['vader_score'] = df['text'].apply(lambda x: sia.polarity_scores(x)['compound'
 X = df[['text', 'vader_score']]
 y = df['label']
 
+
 # step 5: train/test split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=2)
 
-# step 6: set everything up for the pipeline
-text_transformer = CountVectorizer(stop_words='english')
-vader_transformer = FunctionTransformer(lambda x: x.values.reshape(-1, 1), validate=False)
-
-vader_pipeline = Pipeline([
-    ('reshape', FunctionTransformer(lambda x: x.values.reshape(-1, 1), validate=False)),     # bayes doesn't like negatives, fixing that here
-    ('scale', MinMaxScaler())
-])
-
-preprocessor = ColumnTransformer(
-    transformers=[
-        ('text', text_transformer, 'text'),
-        ('vader', vader_pipeline, 'vader_score')
-    ]
-)
-
-# step 7: create the naive bayes model
-model = Pipeline([
-    ('features', preprocessor),
-    ('classifier', MultinomialNB())
-])
-
 # step 8: train it
+model = VaderBayes()
 model.fit(X_train, y_train)
 
 # step 9: predict
